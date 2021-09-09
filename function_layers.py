10858be9f54c6e3b8ecc5dd89d5edf4bde347cc9
octagomez <57689320+octagomez@users.noreply.github.com>
2021-09-08 21:14:58 -0300

    re

--- function_layers.py
+++ function_layers.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-%matplotlib inline
+# %matplotlib inline
 sns.set_context("notebook")
 plt.style.use("fivethirtyeight")
 
